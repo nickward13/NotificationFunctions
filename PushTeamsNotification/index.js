63e34f46e13e58f1c0345a7a53cc3074c83e5df0
Nick Ward <nickward@microsoft.com>
2017-02-14 18:48:07 +1000

    updated to JSON

--- PushTeamsNotification/index.js
+++ PushTeamsNotification/index.js
@@ -3,23 +3,23 @@ module.exports = function (context, req) {
     
     context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
 
-    var querystring = require('querystring');
+    var json = require('JSON');
     var http = require('http');
-    var fs = require('fs');
     
     if (req.query.TaskName || (req.body && req.body.TaskName)) {
         
-        var post_data = querystring.stringify({
-            'text': 'Task completed: ' + (req.query.TaskName || req.body.TaskName) + '!'
+        var post_data = json.stringify({
+            'text': 'test2'
         });
-
+        
         var post_options = {
             host: 'outlook.office.com',
             port: '80',
             path: '/webhook/fb04a14e-4555-4a12-a0a7-e8493af4b26b@72f988bf-86f1-41af-91ab-2d7cd011db47/IncomingWebhook/1c31238a6d1045d39b73112e54809e39/75fbf278-a8ae-48b3-a5e5-c8ee58cad87f',
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Content-Length': Buffer.byteLength(post_data)
             }
         };
 
