be41293c6b08f906f86def309db32af1e6043698
Nick Ward <nickward@microsoft.com>
2017-02-14 18:34:11 +1000

    post message to teams

--- PushTeamsNotification/index.js
+++ PushTeamsNotification/index.js
@@ -3,20 +3,36 @@ module.exports = function (context, req) {
     
     context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
 
-    var request = require("request");
+    var querystring = require('querystring');
+    var http = require('http');
+    var fs = require('fs');
     
     if (req.query.TaskName || (req.body && req.body.TaskName)) {
         
-        request({
-            uri: "https://outlook.office.com/webhook/fb04a14e-4555-4a12-a0a7-e8493af4b26b@72f988bf-86f1-41af-91ab-2d7cd011db47/IncomingWebhook/1c31238a6d1045d39b73112e54809e39/75fbf278-a8ae-48b3-a5e5-c8ee58cad87f",
-            method: "POST",
-            timeout: 1000,
-            form: {
-                text: 'Task completed: ' + (req.query.TaskName || req.body.TaskName) + '!'
+        var post_data = querystring.stringify({
+            'text': 'Task completed: ' + (req.query.TaskName || req.body.TaskName) + '!'
+        });
+
+        var post_options = {
+            host: 'outlook.office.com',
+            port: '80',
+            path: '/webhook/fb04a14e-4555-4a12-a0a7-e8493af4b26b@72f988bf-86f1-41af-91ab-2d7cd011db47/IncomingWebhook/1c31238a6d1045d39b73112e54809e39/75fbf278-a8ae-48b3-a5e5-c8ee58cad87f',
+            method: 'POST',
+            headers: {
+                'accept': 'application/json'
             }
-        }, function(error, response, body){
-            context.log(body);
-        })
+        };
+
+        var post_req = http.request(post_options, function(res){
+            res.setEncoding('utf8');
+            res.on('data', function(chunk){
+                context.log('Response: ' + chunk);
+            });
+        });
+
+        post_req.write(post_data);
+        post_req.end();
+        
     }
     else {
         context.res = {
