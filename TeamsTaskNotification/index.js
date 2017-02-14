588dcea58f4f36498a819acb25bdbc9eca035397
Nick Ward <nickward@microsoft.com>
2017-02-15 09:11:09 +1000

    updated notification text

--- TeamsTaskNotification/index.js
+++ TeamsTaskNotification/index.js
@@ -5,7 +5,7 @@ module.exports = function (context, req) {
         var request = require("request");
 
         var requestData = {
-            "text": (req.query.TaskName || req.body.TaskName) + " is completed."
+            "text": "Task '" + (req.query.TaskName || req.body.TaskName) + "' is completed."
         }
 
         url = "https://outlook.office.com/webhook/fb04a14e-4555-4a12-a0a7-e8493af4b26b@72f988bf-86f1-41af-91ab-2d7cd011db47/IncomingWebhook/1c31238a6d1045d39b73112e54809e39/75fbf278-a8ae-48b3-a5e5-c8ee58cad87f"
