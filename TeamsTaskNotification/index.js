bd40edf9f5364b8d17b1483ad0d5c9b5d42472e5
Nick Ward <nickward@microsoft.com>
2017-02-15 09:08:47 +1000

    send through task text

--- TeamsTaskNotification/index.js
+++ TeamsTaskNotification/index.js
@@ -5,30 +5,29 @@ module.exports = function (context, req) {
         var request = require("request");
 
         var requestData = {
-            "text": "test2"
+            "text": (req.query.TaskName || req.body.TaskName) + " is completed."
         }
 
         url = "https://outlook.office.com/webhook/fb04a14e-4555-4a12-a0a7-e8493af4b26b@72f988bf-86f1-41af-91ab-2d7cd011db47/IncomingWebhook/1c31238a6d1045d39b73112e54809e39/75fbf278-a8ae-48b3-a5e5-c8ee58cad87f"
         
         request({
-            url: url,
-            method: "POST",
-            json: requestData
-        }, 
-        function (responseError, response, responseBody) {
-            if (!error && response.statusCode === 200) {
-                context.res = {
-                    body: "Notification sent"
+                url: url,
+                method: "POST",
+                json: requestData
+            }, 
+                function (error, response, body) {
+                    if (!error && response.statusCode === 200) {
+                        context.res = {
+                            body: "Notification sent"
+                        }
+                    } else {
+                        context.res = {
+                            status: response.statusCode,
+                            body: response.statusText
+                    }
                 }
-            }
-            else {
-                context.res = {
-                    status: response.statusCode,
-                    body: response.statusText
-                }
-            }
-        })
-        
+            })
+
     }
     else {
         context.res = {
